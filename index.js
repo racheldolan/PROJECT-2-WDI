7fc778640fbb4a2eb9878526614272263dc28bba
Rachel Dolan <racheldolan1@gmail.com>
2018-06-02 11:55:45 +0100

    express app set up

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
-
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/views`);
+app.use(expressLayouts);
 
 app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => res.render('pages/home'));
+
 
 app.listen(4000, () => console.log('listening on port 4000'));
