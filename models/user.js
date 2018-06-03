115d7dd08511de07c41d6df330e456f861ec3bd9
Rachel Dolan <racheldolan1@gmail.com>
2018-06-03 13:31:30 +0100

    added user schema to models

--- models/user.js
+++ models/user.js
@@ -1,1 +1,40 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: {type: String, required: true},
+  email: {type: String, unique: true, required: true},
+  password: {type: String, required: true}
+}, {
+  timestamps: true
+});
+
+userSchema.virtual('pictures', {
+  ref: 'Picture',
+  foreignField: 'creator',
+  localField: '_id'
+});
+
+userSchema.methods.validatePassword = function(password){
+  return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.pre('save', function(next){
+  if(this.isModified('password')){
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+});
+
+userSchema.virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(passwordConfirmation){
+    this._passwordConfirmation = passwordConfirmation;
+  });
+
+userSchema.pre('validate', function(next){
+  if(this.isModified('password') && this._passwordConfirmation !== this.password){
+    this.invalidate('password');
+  }
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
