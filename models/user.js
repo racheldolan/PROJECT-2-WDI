c14ba68c76ca5ada5dab60e96ce731ae39fc4fd2
Rachel Dolan <racheldolan1@gmail.com>
2018-06-04 12:31:13 +0100

    added delete button to user page

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   username: {type: String, required: true},
   email: {type: String, unique: true, required: true},
   profile: {type: String},
+  location: {type: String},
   bio: {type: String},
   website: {type: String},
   password: {type: String, required: true}
