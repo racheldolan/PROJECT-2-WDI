22351606f0db02f397e5bee8049d2210b589f5fb
Rachel Dolan <racheldolan1@gmail.com>
2018-08-07 18:04:52 +0100

    altered environment

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
 module.exports = {
   port: process.env.PORT || 4000,
-  dbURI: process.env.MONGODB_RUI || 'mongodb://localhost/mongo-canvas'
+  dbURI: process.env.MONGODB_RUI || 'mongodb://localhost:27017/mongo-canvas'
 };
